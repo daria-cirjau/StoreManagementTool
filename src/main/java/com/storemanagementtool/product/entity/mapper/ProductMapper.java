6a7bce58c7f96867a6b6195cad606cf97731b670
daria-cirjau <dariaaax@yahoo.com>
2025-08-28 20:36:02 +0300

    Create Product mapper to convert entity to dto

---
+++ src/main/java/com/storemanagementtool/product/entity/mapper/ProductMapper.java
@@ -0,0 +1,59 @@
+package com.storemanagementtool.product.entity.mapper;
+
+import com.storemanagementtool.product.entity.Product;
+import com.storemanagementtool.product.entity.dto.ProductDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public Product toEntity(ProductDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        return Product.builder()
+                .name(dto.name())
+                .price(dto.price())
+                .currency(dto.currency())
+                .quantity(dto.quantity())
+                .category(dto.category())
+                .build();
+    }
+
+    public ProductDTO toDto(Product entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return ProductDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .price(entity.getPrice())
+                .currency(entity.getCurrency())
+                .quantity(entity.getQuantity())
+                .category(entity.getCategory())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
+
+    public void applyPatchToEntity(ProductDTO dto, Product entity) {
+        if (dto.name() != null) {
+            entity.setName(dto.name());
+        }
+        if (dto.price() != null) {
+            entity.setPrice(dto.price());
+        }
+        if (dto.currency() != null) {
+            entity.setCurrency(dto.currency());
+        }
+        if (dto.quantity() != null) {
+            entity.setQuantity(dto.quantity());
+        }
+        if (dto.category() != null) {
+            entity.setCategory(dto.category());
+        }
+    }
+
+}

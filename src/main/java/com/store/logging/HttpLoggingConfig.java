9b2ff0979ba1d2f353c279d81910fd2eb0b309e6
daria-cirjau <dariaaax@yahoo.com>
2025-08-31 20:30:41 +0300

    Add logging config

---
+++ src/main/java/com/store/logging/HttpLoggingConfig.java
@@ -0,0 +1,19 @@
+package com.store.logging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class HttpLoggingConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter f = new CommonsRequestLoggingFilter();
+        f.setIncludeClientInfo(true);
+        f.setIncludeQueryString(true);
+        f.setIncludePayload(true);
+        f.setIncludeHeaders(true);
+        f.setMaxPayloadLength(10_000);
+        return f;
+    }
+}
